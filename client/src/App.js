aa692747b91caacec6c52ecb1a01a51904f65a5d
Luis Cuzquen <cuzquenjr@gmail.com>
2018-11-13 17:21:19 -0700

    components and containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Members from './components/members/members'
+import Members from './components/members/members';
+import PostBlog from './components/PostBlog';
+import NavBar from "./components/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Members/>
+        <NavBar/>
+          <PostBlog/>
         </header>
       </div>
     );
