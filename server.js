1302c49b5a2f4aa41af838127f556532f6214784
Unknown <robert.vanarsdale@gmail.com>
2018-11-13 21:17:05 -0500

    updated the server.js file for a better setup

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 var mysql = require('mysql')
+const port = 5000;  
+
+app.get('/blog', (req, res) => {
+	connection.connect();
+	connection.query('SELECT * from WatchesList', function(err, rows, fields) {
+		if (!err) {
+        	res.send(JSON.stringify(rows));
+		} else {
+			console.log('Error while performing Query.');
+		}
+	});
+	connection.end();
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 var connection = mysql.createConnection({
   host: 'blogs-comments.cjyyl4sipsn7.us-east-2.rds.amazonaws.com',
@@ -14,5 +29,17 @@ connection.connect(function(err) {
   console.log('You are now connected...')
 })
 
-
-
+exports.create = function(id, blog, date, username, deleted, title ) {
+    var values = [id, blog, date, username, deleted, title ]
+    
+    db.get().query('INSERT INTO blog (id, blog, date, username, deleted, title) VALUES(?, ?, ?)', values, function(err, result) {
+      if (err) return done(err)
+      done(null, result.insertId)
+    })
+  }
+  exports.getAll = function(done) {
+    db.get().query('SELECT * FROM blog', function (err, rows) {
+      if (err) return done(err)
+      done(null, rows)
+    })
+  }
