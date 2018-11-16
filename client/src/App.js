e7dff6f9a5ccec57cfe71255879a99bb2c23c200
Desrie Horan <desriehoran.casa@gmail.com>
2018-11-15 20:44:27 -0600

    triedlogo

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import PostBlog from './components/PostBlog';
 import NavBar from "./components/Navbar";
 import Blog from "./components/blog/blog";
 import Comments from './components/comments/comments';
+import logo from "./as-we-travel logo.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        img src={logo} alt="team logo"/> 
         <NavBar/>
           <PostBlog/>
         </header>
