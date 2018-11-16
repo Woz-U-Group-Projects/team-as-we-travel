2789f4c7d003f454e26ba1655174a7cdb51da9d9
Unknown <robert.vanarsdale@gmail.com>
2018-11-15 21:41:08 -0500

    added post  function to form working out bugs
    Uncaught (in promise) SyntaxError: Unexpected token S in JSON at position 0 error, working this out

--- client/src/components/PostBlog/index.js
+++ client/src/components/PostBlog/index.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 
 class PostBlog extends Component {
-
+    constructor() {
+        super();
+        this.state = { user: {} };
+        this.onSubmit = this.handleSubmit.bind(this);
+      }
+      handleSubmit(e) {
+        e.preventDefault();
+        var self = this;
+        // On submit of the form, send a POST request with the data to the server.
+        fetch('/blogPost', { 
+            method: 'POST',
+            data: {
+              userName: self.refs.userName,
+              blog: self.refs.blog,
+              title: self.refs.title
+            }
+          })
+          .then(function(response) {
+            return response.json()
+          }).then(function(body) {
+            console.log(body);
+          });
+      }
     render() {
         return (
-            <form>
-                <div>
-                    <label>
-                        Username: 
-                    
-            </label>
+
+            
         
-                    <input></input>
-                </div>
+        
+            <form onSubmit={this.onSubmit}>
                 <div>
-                    <label>
-                        Blog: 
-                </label>
-                    <textarea rows="15" cols="50"></textarea>
+                    <label>Username: </label>
+                            <input type="text" placeholder="Username" ref="userName"/>
+                             
+                                <br/>
+
+                    <label>Title: </label>
+                            <input type="text" placeholder="Blog Title" ref="title"/>         
+
+                </div>
+
+                <div>
+                    <label>Blog: </label>
+                        <textarea rows="15" cols="50" type="text" placeholder="Blog text goes here" ref="blog"/>
                 </div>
                 <div>
                     <button>Post</button>
