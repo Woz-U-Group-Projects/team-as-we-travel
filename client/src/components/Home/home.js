a455968fcc2d10606e7a162f943d6dc1b6591717
Desrie Horan <desriehoran.casa@gmail.com>
2018-12-04 21:16:58 -0600

    added images to home page

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -4,42 +4,60 @@ import './home.css';
 
 
 class Home extends Component {
-    render() {
-     
-      return (
-        
-        <div className="home">
-            <header className="masthead text-center text-white d-flex">
-      <div className="container my-auto">
-        <div className="row">
-          <div className="col-lg-10 mx-auto">
-            <h1 className="text-uppercase">
-              <strong>Welcome to As We Travel!</strong>
-            </h1>
-            <hr/>
-         </div>
-          <div className="col-lg-8 mx-auto">
-            <p className="text-faded mb-5" >Join us and other travelers from around the world!</p>
-            <p className="text-faded mb-10">Below are the following technologies used to build this web app.</p>
-          </div>
+   render() {
+
+     return (
+
+       <div className="home">
+           <header className="masthead text-center text-white d-flex">
+     <div className="container my-auto">
+       <div className="row">
+         <div className="col-lg-10 mx-auto">
+           <h1 className="text-uppercase">
+             <strong>Welcome to As We Travel!</strong>
+           </h1>
+           <hr/>
         </div>
-      </div>
-    </header>
-    <div className="gridRow">
-    <div classCol xs={6} md={4}>
-    <img alt ="img" src="images\react.png"/>
-    </div>
-    <div classCol xs={6} md={4}>
-    <img alt ="img" src="images\js.png"/>
-    </div>
-    <div classCol xs={6} md={4}>
-    <img alt ="img" src="images\html5.png"/>
-    </div>
+         <div className="col-lg-8 mx-auto">
+           <p className="text-faded mb-5" >Join us and other travelers from around the world!</p>
+           <p className="text-faded mb-10">Below are the following technologies used to build this web app.</p>
+         </div>
+       </div>
+     </div>
+
+
+
+<div className="row">
+ <div className="column">
+   <img src="images\react.png" alt="img"/>
+ </div>
+ <div className="column">
+   <img src="images\js.png" alt="img" />
+ </div>
+ <div className="column">
+   <img src="images\html5.png" alt="img" />
+ </div>
+ <div className="column">
+   <img src="images\css.png" alt="img" />
+ </div>
+ <div className="column">
+   <img src="images\sql.png" alt="img" />
+ </div>
+ <div className="column">
+   <img src="images\express1.png" alt="img" />
+ </div>
+  
 </div>
+
+
+
+
+   </header>
+
 </div>
-               
-    );
-  }
+
+   );
+ }
 };
 
- export default Home;
+export default Home;
